9188e53032180b9703af2f259135e2332eb513c8
Kevin Hjelden <fryguy@burntpopcorn.net>
2023-07-09 23:56:40 -0700

    Add About page

---
+++ src/about.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { Message, Container } from "semantic-ui-react";
+
+export const About = () =>
+    <Container>
+        <Message>
+            This website uses trademarks and/or copyrights owned by Paizo Publishing, LLC, which are used
+            under Paizo's Community Use Policy. We are expressly prohibited from charging you to use or
+            access this content. This website is not published, endorsed, or specifically approved by Paizo
+            Publishing. For more information about Paizo's Community Use Policy, please visit
+            paizo.com/communityuse. For more information about Paizo Publishing and Paizo products, please
+            visit paizo.com.
+        </Message>
+    </Container>
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -4,30 +4,52 @@ import { Provider } from "react-redux";
 import configureStore from "./store";
 import * as serviceWorker from './serviceWorker'
 import { createBrowserHistory } from 'history'
-
+import { About } from './about'
+import './site.css';
 
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route, Redirect } from "react-router";
+import { NavLink } from "react-router-dom";
 import EncounterBuilder from "./encounter/EncounterBuilder";
-import { Message } from "semantic-ui-react";
+import { Menu, Header, Container } from "semantic-ui-react";
 
 export const history = createBrowserHistory({ basename: '/pf2utils' });
 
 const store = configureStore(history);
 
+const Nav = (props: any) => (
+	<NavLink
+		exact
+		{...props}
+		activeClassName="active"
+	/>
+);
+const Heading = () => (
+    <Menu inverted size='large' fixed='top' style={{ margin: 0 }}>
+        <Menu.Item>
+            <Header inverted>Five Goblins Encounter Builder</Header>
+        </Menu.Item>
+        <Menu.Item as={Nav} to='/encounter' position='right'>Encounter Builder</Menu.Item>
+        <Menu.Item as={Nav} to='/about'>About</Menu.Item>
+    </Menu>
+);
+
 const Root = () => (
-    <div>
-        <Provider store={store}>
-            <ConnectedRouter history={history}>
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Heading/>
+            <Container fluid style={{ marginTop: '5rem' }}>
                 <Switch>
                     <Route path="/" exact render={() => (<Redirect to="/encounter"/>)}/>
                     <Route path="/encounter" render={() => <EncounterBuilder />} />
+                    <Route path="/about" render={() => <About />} />
                 </Switch>
-            </ConnectedRouter>
-        </Provider>
-        <Message>This website uses trademarks and/or copyrights owned by Paizo Publishing, LLC, which are used under Paizo's Community Use Policy. We are expressly prohibited from charging you to use or access this content. This website is not published, endorsed, or specifically approved by Paizo Publishing. For more information about Paizo's Community Use Policy, please visit paizo.com/communityuse. For more information about Paizo Publishing and Paizo products, please visit paizo.com.</Message>
-    </div>
+            </Container>
+        </ConnectedRouter>
+    </Provider>
 );
+
+
 
 render(<Root />, document.getElementById("root"));
 
